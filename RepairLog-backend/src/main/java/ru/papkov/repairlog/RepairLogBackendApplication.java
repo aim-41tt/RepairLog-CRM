06890d69390b61238dfd5d6825e55c69c817e9e2
aim_41tt <0505mustik@gmail.com>
2026-01-05 13:27:28 +0300

    first commit

---
+++ RepairLog-backend/src/main/java/ru/papkov/repairlog/RepairLogBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.papkov.repairlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairLogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepairLogBackendApplication.class, args);
+	}
+
+}
